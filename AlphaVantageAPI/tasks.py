f83564e3c9a082259fe191e0c16b91a2aee672fd
maliibrahim <maliibrahim@getgroup.com>
2021-08-29 10:25:07 +0400

    Django Alpha vantage Rest API

--- AlphaVantageAPI/tasks.py
+++ AlphaVantageAPI/tasks.py
@@ -5,7 +5,7 @@ from .exchange_rate_method import get_bitcoin_usd_exchange_rate
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(get_bitcoin_usd_exchange_rate, 'interval', minutes=5)
+    scheduler.add_job(get_bitcoin_usd_exchange_rate, 'interval', hours=1)
     scheduler.start()
 
     # Shut down the scheduler when exiting the app
